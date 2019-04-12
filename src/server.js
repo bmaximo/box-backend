29a1caab1677f54ca9f8d54f7c8430b4252384e6
Bárbara Castilho Máximo <barbaracm@pmi.intra>
2019-04-12 07:39:05 -0300

    create boxes and files and the routes

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express"); 
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+mongoose.connect(
+    'mongodb+srv://barbara:barbara@cluster0-ifzga.mongodb.net/barbaradb?retryWrites=true', 
+    {
+        useNewUrlParser: true
+    }
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+app.listen(3333);
